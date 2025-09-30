6b5f3d3e0b520f626392862991d737c8852393ef
rohit singh <codex.rohit.singh@gmail.com>
2025-10-01 02:09:23 +0530

    having signed and unsigned

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     // println!("Hello, this is my first rust project ");
-    let x :i8= 5; 
-    println!("{}",x)
+    let x :i8= 5; //i means signed and it can both postive and negative 
+    let y :u8 =10;  //unsigned mean you can only use positive 
+    println!("{}",x);
+    println!("{}",y)
 }
