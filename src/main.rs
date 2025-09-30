f4da0455fc41a37518d52d741753fa74ec1f295e
rohit singh <codex.rohit.singh@gmail.com>
2025-10-01 02:27:50 +0530

    if you have a unused variable use a _a to describe the variabel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 fn main() {
     // println!("Hello, this is my first rust project ");
-    let x :i8= 5; //i means signed and it can both postive and negative 
-    let y :u8 =10;  //unsigned mean you can only use positive 
-    println!("{}",x);
-    println!("{}",y)
+    // let x :i8= 5; //i means signed and it can both postive and negative 
+    // let y :u8 =10;  //unsigned mean you can only use positive 
+    // let z :u32= 1000;
+    // let a :f32=1000.001; //this is for float 
+    // println!("float is  : {}",a);
+    // println!("{}",x);
+    // println!("{}",y);
+    // println!("{}",z);
+    let mut x :i8 = 5;
+
+    // for i : i32 in 0..1000 {
+    //     x= x+10;
+    // }
+    // for i: i32 in 0..1000{
+    //     x=x+10;
+    // }
+    for _i in 0..1000{
+        x=x+100;
+    }
+    print!("{}",x)
 }
