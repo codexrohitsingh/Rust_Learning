4a9ddc7d1a634d69357031ebaeeed0b7ba6f40ff
rohit singh <codex.rohit.singh@gmail.com>
2025-10-01 15:23:57 +0530

    12

--- src/main.rs
+++ src/main.rs
@@ -34,12 +34,38 @@ fn main() {
     // if is_male && is_above_18 {
     //     print!("It is a real male ")
     // }
-    let greeting =String::from ("hello World");
-    println!(" {}",greeting);
-    let char1 =greeting.chars().nth(1);
-    match char1 {
-        Some(c)=>println!("{}",c),
-        None=>println!("No character at 1000th")
-    }
+    // let greeting =String::from ("hello World");
+    // println!(" {}",greeting);
+    // let char1 =greeting.chars().nth(10);
+    // print!("{}",char1.unwrap());
+
+    // match char1 {
+    //     Some(c)=>println!("{}",c),
+    //     None=>println!("No character at 1000th")
+    // }
     // print!("{}",greeting.chars().nth(10));
+
+    // Conditional and loops
+    // let is_even :bool = false;
+    // if is_even{
+    //     println!{"This is even "}
+    // }else{
+    //     println!("This is Odd")
+    // }
+    // for i in 0..10{
+    //     println!("{}",i)
+    // }
+    let first_sentence = String::from("This is a sentence ");
+    let find_first_word=get_first_word(first_sentence);
+    fn get_first_word(first_sentence: String)->String{
+        let mut word = String::from("");
+        for char in first_sentence.chars(){
+            word.push_str(char.to_string().as_str());
+            if word==" "{
+                break;
+        }
+    }
+    return word;
+}
+println!("{}",find_first_word)
 }
