437573bed48b206b1da882ebf40c2d5eed30c1fa
rohit singh <codex.rohit.singh@gmail.com>
2025-10-03 01:38:25 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -210,8 +210,8 @@
 //     println!("{}",s1);
 
 // }
-fn main(){
-    let mut s1 =String::from("Hello");
-    s1.push_str("World");
-    println!("{}",s1);
-}
+// fn main(){
+//     let mut s1 =String::from("Hello");
+//     s1.push_str("World");
+//     println!("{}",s1);
+// }
