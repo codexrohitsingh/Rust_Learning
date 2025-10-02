e319f5ad5540fd52706608cfd6079ac8b047a8d2
rohit singh <codex.rohit.singh@gmail.com>
2025-10-02 23:08:39 +0530

    research

--- src/main.rs
+++ src/main.rs
@@ -141,9 +141,11 @@
 
 // Write a program to find the reverse of a number 123; 
 fn main (){
-     find_reverse();
+    let a = 10;
+    find_reverse(a);
 }
 
-fn find_reverse(){
+fn find_reverse(a:f32){
+    println!("{}",a%10);
     
 }
