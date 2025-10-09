5a29f938461a8a6e31c7aa2fee0fc59fa3e1c3f7
rohit singh <codex.rohit.singh@gmail.com>
2025-10-09 17:27:10 +0530

    1

--- rust_part2/src/main.rs
+++ rust_part2/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, rohit singh ");
-}
+// fn main() {
+//     println!("Hello, rohit singh ");
+// }
+ fn main(){
+let a :i8 = 24;
+let b = 32;
+println!("{}",a+b);
+ }
