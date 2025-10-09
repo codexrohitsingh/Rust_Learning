ba17e9e1d936d688fd922d61ca16bb1f3cb31794
rohit singh <codex.rohit.singh@gmail.com>
2025-10-09 17:37:37 +0530

    5th

--- rust_part2/src/main.rs
+++ rust_part2/src/main.rs
@@ -22,8 +22,18 @@
 
 
 // print the number from 1 to 10 
+// fn main(){
+//     for i in 1..11{
+//         println!("{}",i);
+//     }
+// }
+
+// Find the factorial of a number 
 fn main(){
-    for i in 1..11{
-        println!("{}",i);
+    let mut a = 1;
+    let n = 5;
+    for i in 1..n+1{
+        a = a*i;
     }
+    println!("{}",a);
 }
