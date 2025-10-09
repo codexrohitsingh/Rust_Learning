d51e5283a1cfc436bd832293fad9e2506c0777e9
rohit singh <codex.rohit.singh@gmail.com>
2025-10-09 17:31:40 +0530

    .

--- rust_part2/src/main.rs
+++ rust_part2/src/main.rs
@@ -1,8 +1,21 @@
 // fn main() {
 //     println!("Hello, rohit singh ");
 // }
- fn main(){
-let a :i8 = 24;
-let b = 32;
-println!("{}",a+b);
+//  fn main(){
+// let a :i8 = 24;
+// let b = 32;
+// println!("{}",a+b);
+//  } 
+
+
+// Check if a number is even or odd 
+
+fn main(){
+let a = 23;
+ if a%2==0 {
+    print!("this is even");
  }
+ else{
+    println!("this is odd");
+ }
+}
